80c2f5415f55c110ed8610be0da25357f4f7266a
Abhsihek748 <abhishek.sharma9484@gmail.com>
2021-10-27 13:31:14 +0530

    comments added

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ deploy : https://csvtojsonnodejs.herokuapp.com
 
 in this project i have created a nodejs server which convers some csv data and parse it into Json/object form and gives us relevant data when we query it using get request .
 
-eg : https://csvtojsonnodejs.herokuapp.com/?country=BR
+eg : https://csvtojsonnodejs.herokuapp.com/?country=MF
 
 
- output  : {"data":{"IN":{"Code":"IN","Name":"India","Native":"भारत","Phone":"91","Continent":"Asia","Capital":"New Delhi","Currency":"INR","Languages":{"code":"hi","name":"Hindi"}}}}
+ {"data":{"MF":{"Code":"MF","Name":"Saint Martin","Native":"Saint-Martin","Phone":"590","Continent":"North America","Capital":"Marigot","Currency":"EUR","Languages":[{"code":"en","name":"English"},{"code":"fr","name":"French"},{"code":"nl","name":"Dutch"}]}}}
 
-// new branch
+ just copy the repo and to npm index.js or nodemon then the server will start at port http://localhost:8081 ;
+
+  postman api 👍 // this is a test api http://localhost:8081/?country=MF get request 
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require("express"); // to start the server
 const app = express();
-const csv = require('csv-parser');
+const csv = require('csv-parser'); // parse the csv file
 const fs = require('fs');
 app.use(express.json());
 
-const csvToData = async (data)=>{
+const csvToData = async (data)=>{          // read the csv file and parse it
    return new Promise((resolve, reject)=>{
   let obj = [];
   fs.createReadStream(data)
@@ -21,7 +21,9 @@ const csvToData = async (data)=>{
 
 let languages=[],countries=[], continents=[] , counter = 0;
 
-csvToData('countries.csv').then((data) => {
+// reading all the acailable csv files from system
+
+csvToData('countries.csv').then((data) => {    
     countries = data;
     counter = counter + 1;
 });
@@ -35,7 +37,7 @@ csvToData('continents.csv').then((data) => {
     counter += 1;
 });
 
-const find = (data, value)=>{
+const find = (data, value)=>{      // helps to find the value in the data where some object of data have code == value and returns the object
     // console.log(value);
     for(let i in data ){
         if(data[i]["Code"]== value)
@@ -45,38 +47,38 @@ const find = (data, value)=>{
     }
 }
 
-app.get('/',function(req,res){
+app.get('/',function(req,res){    // route
     let data = {};
- let country = req.query.country ;
+ let country = req.query.country ;        // reading the query
  if(country){
- let obj = find(countries, country);
- let language ={};
+ let obj = find(countries, country); // finding the country object
+ let language ={}; 
  let codes = obj["Languages"];
- console.log(codes);
- if(codes.includes(',')){
+ console.log(codes);                   // check if one or more languages
+ if(codes.includes(',')){                      // run for more than 1 language
      language = [];
      codes = codes.split(",") ;
      for(let code of codes){
-        let name = find(languages , code)["Name"];   
+        let name = find(languages , code)["Name"];    // find the name of language from its code
         let obj = {code , name};
         language.push(obj);
      }
      obj["Languages"] = language ;
  }
- else{
+ else{                                 // run for 1 language
  language["code"] = codes ;
- language["name"] = find(languages , language.code)["Name"];
+ language["name"] = find(languages , language.code)["Name"]; // find the name of language from its code
   obj["Languages"] = language ;
  }
 
   
-  data[country] = obj;
+  data[country] = obj;     
  }
 //  console.log(data);
-  res.json({"data" :data });
+  res.json({"data" :data });   //  sends the parsed data
 })
 
 
-app.listen(process.env.PORT||8081, function () {
+app.listen(process.env.PORT||8081, function () {      //  server listening
     console.log("server started");
 })
